15d9c13ee90e547762ef8c04cc53628bf6b774cd
M Miller <melissa.miller@pantheon.io>
2024-03-15 17:35:38 -0700

    Remove search bar and examples link from navbar

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,14 +1,16 @@
-import { Footer, PreviewRibbon } from '@pantheon-systems/nextjs-kit';
-import Link from 'next/link';
-import { Searchbar } from './searchbar';
-import { Container, Navbar, NavMenu } from '@pantheon-systems/pds-toolkit-react';
+import { Footer, PreviewRibbon } from "@pantheon-systems/nextjs-kit";
+import Link from "next/link";
+import {
+  Container,
+  Navbar,
+  NavMenu,
+} from "@pantheon-systems/pds-toolkit-react";
 
-import { pdsConfig } from '../pds.config';
+import { pdsConfig } from "../pds.config";
 
 export default function Layout({ children, footerMenu, preview }) {
   const mainMenuItems = [
-    { linkContent: <Link href='/articles'>Articles</Link> },
-    { linkContent: <Link href='/examples'>Examples</Link> },
+    { linkContent: <Link href="/articles">Articles</Link> },
   ];
 
   const footerMenuItems = footerMenu?.map(({ path, label }) => ({
@@ -18,36 +20,34 @@ export default function Layout({ children, footerMenu, preview }) {
   }));
 
   return (
-    <div className='min-h-screen max-h-screen min-w-screen max-w-screen flex flex-col overflow-x-hidden'>
+    <div className="min-h-screen max-h-screen min-w-screen max-w-screen flex flex-col overflow-x-hidden">
       {preview && <PreviewRibbon />}
 
       <Navbar
         mobileMenuMaxWidth={pdsConfig.mobileMenuBreakpoint}
-        containerWidth='standard'
-        logoDisplay='sub-brand'
+        containerWidth="standard"
+        logoDisplay="sub-brand"
         logoSubBrand={{
           text: pdsConfig.siteName,
-          link: <Link href='/'>Home</Link>,
+          link: <Link href="/">Home</Link>,
         }}
       >
         <NavMenu
-          slot='items-right'
-          ariaLabel='Main Navigation'
+          slot="items-right"
+          ariaLabel="Main Navigation"
           menuItems={mainMenuItems}
           mobileMenuMaxWidth={pdsConfig.mobileMenuBreakpoint}
         ></NavMenu>
-        <Searchbar slot='items-right' />
       </Navbar>
 
-      <main className='mb-auto'>
-        <Container width="standard">
-          {children}
-        </Container>
+      <main className="mb-auto">
+        <Container width="standard">{children}</Container>
       </main>
       <Footer footerMenuItems={footerMenuItems}>
-        <span className='mx-auto'>
-          © {new Date().getFullYear()} Built with <a href='https://nextjs.org/'>Next.js</a> and{' '}
-          <a href='https://pantheon.io/'>Pantheon Content Cloud</a>
+        <span className="mx-auto">
+          © {new Date().getFullYear()} Built with{" "}
+          <a href="https://nextjs.org/">Next.js</a> and{" "}
+          <a href="https://pantheon.io/">Pantheon Content Cloud</a>
         </span>
       </Footer>
     </div>
