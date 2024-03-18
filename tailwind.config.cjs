4dfd229231444daf0079192985dba97e8eb8b090
M Miller <melissa.miller@pantheon.io>
2024-03-18 16:19:20 -0700

    Update tailwind.config.cjs
    Begin replacing TW defaults with PDS values

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,69 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    borderRadius: {
+      none: "0px",
+      DEFAULT: "0.1875rem",
+      container: "0.375rem",
+    },
+    fontSize: {
+      xs: ["0.694rem", { lineHeight: "1rem" }],
+      sm: ["0.833rem", { lineHeight: "1.25rem" }],
+      base: ["1rem", { lineHeight: "1.5rem" }],
+      lg: ["1.2rem", { lineHeight: "1.75rem" }],
+      xl: ["1.44rem", { lineHeight: "1.75rem" }],
+      "2xl": ["1.728rem", { lineHeight: "2rem" }],
+      "3xl": ["2.074rem", { lineHeight: "2.25rem" }],
+      "4xl": ["2.488rem", { lineHeight: "2.5rem" }],
+      "5xl": ["2.986rem", { lineHeight: "1" }],
+      "6xl": ["3.583rem", { lineHeight: "1" }],
+    },
+    fontWeight: {
+      normal: "400",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+    },
+    letterSpacing: {
+      tight: "-0.02em",
+      normal: "0em",
+      wide: "0.02em",
+      wider: "0.04em",
+      widest: "0.06em",
+    },
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            p: {
+              marginBlockEnd: "1rem",
+            },
+            h1: {
+              fontSize: "2.986rem",
+              fontWeight: "700",
+            },
+            h2: {
+              fontSize: "2.074rem",
+              marginBlockEnd: "1.25rem",
+              marginBlockStart: "2.441rem",
+              fontWeight: "700",
+            },
+            h3: {
+              fontSize: "1.728rem",
+              marginBlockEnd: "1.25rem",
+              marginBlockStart: "2.441rem",
+              fontWeight: "700",
+            },
+            h4: {
+              fontSize: "1.44rem",
+              marginBlockEnd: "1.25rem",
+              marginBlockStart: "1.953rem",
+              fontWeight: "700",
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
