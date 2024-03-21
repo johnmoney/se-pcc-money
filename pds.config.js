a2ad9d1353953d77d40b6106bc396c2082812e2f
M Miller <melissa.miller@pantheon.io>
2024-03-20 20:41:29 -0700

    Update pds.config.js

--- pds.config.js
+++ pds.config.js
@@ -2,5 +2,5 @@ export const pdsConfig = {
   siteName: "Content Cloud Demo",
   mobileMenuBreakpoint: 768, // If the viewport is below this px width, the mobile menu will be shown.
   tableOfContentsHeadingCount: 3, // How many h2s need to be in the content for the TOC to show.
-  homePageHeading: "Welcome", // The heading for the home page.
+  homePageHeading: "Welcome to PCC", // The heading for the home page.
 };
