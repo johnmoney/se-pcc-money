a1058c60b5022650ed077ee95b36c01a2e427177
M Miller <melissa.miller@pantheon.io>
2024-03-20 18:35:24 -0700

    Remove nextjs-kit/style.css
    This was causing style conflicts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,13 @@
-import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleTagManager } from "@next/third-parties/google";
 
-import '../styles/globals.css';
-import '@pantheon-systems/nextjs-kit/style.css';
-import '@pantheon-systems/pds-toolkit-react/_dist/css/pds-core.css';
+import "../styles/globals.css";
+import "@pantheon-systems/pds-toolkit-react/_dist/css/pds-core.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <GoogleTagManager gtmId='G-WC96XDYR3H' />
+      <GoogleTagManager gtmId="G-WC96XDYR3H" />
     </>
   );
 }
