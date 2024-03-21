953990f6f3eb3bcd3ca84f0df81c05538d7071f9
M Miller <melissa.miller@pantheon.io>
2024-03-20 20:18:12 -0700

    testing built styles

--- components/page-header.jsx
+++ components/page-header.jsx
@@ -1,7 +1,7 @@
 export default function PageHeader({ title }) {
   return (
-    <header className="pds-spacing-mar-block-end-2xl pds-spacing-pad-block-start-4xl">
-      <h1 className="pds-ts-5xl text-center">{title}</h1>
+    <header className="pds-spacing-mar-block-end-4xl pds-spacing-pad-block-start-4xl">
+      <h1 className="pds-ts-5xl text-center font-bold">{title}</h1>
     </header>
   );
 }
